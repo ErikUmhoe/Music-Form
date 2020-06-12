4baeee6dd43facf1ad8574cb7449d221b776a00d
ErikUmhoe <98eriku@gmail.com>
2020-06-11 19:59:33 -0500

    Add project files.

---
+++ db music/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(db_music.Startup))]
+namespace db_music
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
